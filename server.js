657a4db576f7115f4e14b4719b9f538985d3ad38
SethSenpai <pimwing@gmail.com>
2018-12-22 23:47:30 +0100

    nightly, tab swaps now function. TODO: sheet interaction

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const Nightmare = require('nightmare');
-const nightmare = Nightmare();
+
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
@@ -21,21 +21,73 @@ app.get('/', (req,res) => {
 
 app.get('/charSheet', (req,resp) => {
     const selector = ".ct-character-sheet__inner";
-    var data;
+    const nightmare = Nightmare({show:true}); //set show false to disable popup
+
+    var body;
+    var actions;
+    var spells;
+    var equipment;
+    var features;
+    var creatures;
+
     console.log(`x: ${req.query.w.x}, y: ${req.query.w.y}`);
     console.log(`getting character sheet request`);
-    nightmare.goto("https://www.dndbeyond.com/profile/SethSenpai/characters/4521436")
+    nightmare.goto('https://www.dndbeyond.com/characters/4521436')
     .viewport(parseInt(req.query.w.x),parseInt(req.query.w.y))
     .wait(selector)
     .evaluate(() => document.querySelector('.ct-character-sheet__inner').innerHTML)
-    .end()
     .then(datab =>{
-        resp.send(datab);
-        console.log(datab);
+        //save body
+        body = datab;
+        console.log(`Send sheet data`);
+        nightmare.evaluate(() => document.querySelector('.ct-tab-list__content').innerHTML)
+        .then(dataa => {
+            //saved actions
+            actions = dataa;
+            console.log(`grabbed actions`);
+            nightmare.click('.ct-primary-box__tab--equipment')
+            .wait('.ct-equipment')
+            .evaluate(()=> document.querySelector('.ct-tab-list__content').innerHTML)
+            .then(datae =>{
+                //saved equipment
+                equipment = datae;
+                console.log(`grabbed equipment`);
+                nightmare.click('.ct-primary-box__tab--features')
+                .wait('.ct-features')
+                .evaluate(()=> document.querySelector('.ct-tab-list__content').innerHTML)
+                .then(dataf => {
+                    //saved features
+                    features = dataf;
+                    console.log(`grabbed features`);
+                    nightmare.click('.ct-primary-box__tab--creatures')
+                    .wait('.ct-creatures')
+                    .evaluate(()=> document.querySelector('.ct-tab-list__content').innerHTML)
+                    .then(datac => {
+                        //saved creatures
+                        creatures = datac;
+                        console.log(`grabbed creatures`);
+                        nightmare.click('.ct-primary-box__tab--spells')
+                        .wait('.ct-spells')
+                        .evaluate(() => document.querySelector('.ct-tab-list__content').innerHTML)
+                        .end()
+                        .then(datas =>{
+                            //saved spells
+                            spells = datas;
+                            console.log(`grabbed spells`);
+                            var data = {b:body,a:actions,s:spells,e:equipment,f:features,c:creatures};
+                            resp.send(data);
+                        })
+                        .catch(error =>{
+                            console.log(`prob not a spellcaster eh: ${error}`);
+                            var data = {b:body,a:actions,s:spells,e:equipment,f:features,c:creatures};
+                            resp.send(data);
+                        })
+                    })
+                })
+            })
+        })
     })
-    .catch(error => {
-        console.error("failed",error);
-    })
+    
 
     //console.log(data);
     //resp.send(data);
--- www/localFunctions.js
+++ www/localFunctions.js
@@ -2,7 +2,53 @@ function getCharSheet(login, charName, windowSize){
     $.get('/charSheet',{w:windowSize}, (data)=>{
         console.log(`requesting character sheet`);
         $('#charSheet').empty();
-        $('#charSheet').append(data);
+        $.when( $('#charSheet').append(data.b) )
+            .done(() => {
+                $('.ct-primary-box__tab--actions')
+                .click(()=>{swapCharTab(0);});
+                $('.ct-primary-box__tab--spells')
+                .click(()=>{swapCharTab(1);});
+                $('.ct-primary-box__tab--equipment')
+                .click(()=>{swapCharTab(2);});
+                $('.ct-primary-box__tab--features')
+                .click(()=>{swapCharTab(3);});
+                $('.ct-primary-box__tab--creatures')
+                .click(()=>{swapCharTab(4);});
+            }
+            );
+        
+
+        actionsDom = data.a;
+        spellsDom = data.s;
+        equipmentDom = data.e;
+        featuresDom = data.f;
+        creaturesDom = data.c;
         console.log(data);
     })
+}
+
+function swapCharTab(n){
+    console.log(`swapping content to ${n}`);
+    switch(n){
+        case 0:
+            $('.ct-tab-list__content').empty();
+            $('.ct-tab-list__content').append(actionsDom);
+        break;
+        case 1:
+            $('.ct-tab-list__content').empty();
+            $('.ct-tab-list__content').append(spellsDom);
+        break;
+        case 2:
+            $('.ct-tab-list__content').empty();
+            $('.ct-tab-list__content').append(equipmentDom);
+        break;
+        case 3:
+            $('.ct-tab-list__content').empty();
+            $('.ct-tab-list__content').append(featuresDom);
+        break;
+        case 4:
+            $('.ct-tab-list__content').empty();
+            $('.ct-tab-list__content').append(creaturesDom);
+        break;
+    }
 }
